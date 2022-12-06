2738ff8b81012c2eae8286a9b2be773241fb5b4e
RobertoAnguloDeveloper <rangulot@outlook.com>
2022-12-05 22:25:24 -0500

    /routes/index.js models added

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,13 +2,17 @@ const express = require("express");
 
 // importamos las rutas
 const categories = require("./Categories");
+const boats = require("./Boats");
+const clients = require("./Clients");
 const admins = require("./Admins");
 const scores = require("./Scores");
 
 const routerApi = (app) => {
     const router = express.Router();
     app.use("/api", router);
+    app.use("/api/boats", boats);
     app.use("/api/categories", categories);
+    app.use("/api/clients", clients);
     app.use("/api/admins", admins);
     app.use("/api/scores", scores);
 };
