6a84b8f11ba6097d4875348f73be5987fc38c42a
Roarco <sebuscaroberth@gmail.com>
2022-12-06 21:54:59 -0500

    Implementando middlewares para el manejo de errores y validaciones de datos, a demas se instalo libreria @happi/boom para gestionar los mensajes de errores enviados al cliente

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ const app = express();
 const routerApi = require('./routes/index');
 const config = require('./config/config');
 const PORT = config.port;
+const { logErrors, boomErrorHandler, errorHandler } = require('./middlewares/errorHandler');
 
 // Middlewares
 app.use(express.json());
 
 // Routes
 routerApi(app);
+
+// Error handlers
+app.use(logErrors);
+app.use(boomErrorHandler);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}...`)
