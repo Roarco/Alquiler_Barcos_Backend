f99b8c3e649445aeb620249a7c13ef3f2974b292
Roarco <sebuscaroberth@gmail.com>
2022-12-06 22:45:04 -0500

    Implementando crud completo de categories, incluyendo validacion de datos y manejo de errores

--- src/models/Categories.js
+++ src/models/Categories.js
@@ -9,6 +9,7 @@ const CategorySchema = new Schema({
   },
   description: {
     type: String,
+    required: true,
   },
 });
 
--- src/services/Categories.js
+++ src/services/Categories.js
@@ -1,13 +1,55 @@
 const mongoose = require("../lib/mongo");
+const boom = require("@hapi/boom");
 const Categories = require("../models/Categories");
 
 class CategoryServices {
-    constructor() {
-    }
+    constructor() { }
 
     // Get all categories
     async getCategories() {
         return await Categories.find();
+    }
+
+    // Get category by id
+    async getCategory(id) {
+        const category = await Categories.findById(id);
+
+        if (!category) {
+            throw boom.notFound("Category not found");
+        }
+        return category;
+    }
+
+    // create category
+    async createCategory(category) {
+        const newCategory = new Categories(category);
+        return await newCategory.save();
+    }
+
+    // update category
+    async updateCategory(id, category) {
+        const findCategory = await this.getCategory(id);
+
+        if (!findCategory) {
+            throw boom.notFound("Category not found");
+        }
+
+        const updatedCategory = await Categories.findByIdAndUpdate(id, category, {
+            new: true,
+        });
+        return updatedCategory;
+    }
+
+    // delete category
+    async deleteCategory(id) {
+        const category = await this.getCategory(id);
+
+        if (!category) {
+            throw boom.notFound("Category not found");
+        }
+
+        await Categories.findByIdAndRemove(id);
+        return { message: "Category deleted" };
     }
 }
 
